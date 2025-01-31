bd338b0167a6b1a81727e3abb0a1d17a0d917b7f
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2025-01-31 13:59:05 +0100

    Leer archivo

--- EquiposDeFootball/Program.cs
+++ EquiposDeFootball/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace EquiposDeFootball
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Users\David\Desktop\esplai\ejercicios\programas\EquiposDeFootball\EquiposDeFootball\Equipos.txt";
+
+            // Leer todo el archivo
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                if (line != "")
+                {
+                    if (line[0] == '*') // Si la línea empieza por *
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+
+
         }
     }
 }
